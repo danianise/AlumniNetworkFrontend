9e9d6bc4a5f9a1293bf9567a3975f5024daa38c4
danianise <danianisehoey@gmail.com>
2022-08-23 21:56:04 -0400

    Change to api url vars for netlify

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path=require('path')
-
-module.exports = {
-    plugins: [
-        new Dotenv({
-          systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
-        })
-      ]
-}
